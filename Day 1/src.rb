361874127a7386acd8a3e0ce94cf83a3e2b04574
MattMcHatton <53062304+MattMcHatton@users.noreply.github.com>
2022-12-01 06:25:52 -0600

    day one complete

--- Day 1/src.rb
+++ Day 1/src.rb
@@ -1,1 +1,23 @@
-print "Hello World!"
+elf_cals_list = []
+current_cals = 0
+
+File.readlines("data.txt").each do |line|
+    if line == "\n"
+        elf_cals_list.push(current_cals)
+        current_cals = 0
+    else
+        current_cals += line.to_i
+    end
+end
+
+# star 1
+puts elf_cals_list.max
+
+current_cals = 0
+elf_cals_list.max(3).each do |cals|
+    current_cals += cals
+end
+
+#star 2
+puts current_cals
+
